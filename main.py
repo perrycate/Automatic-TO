e73074a164f2c56780bb8077e62ad13c5a9b350e
Perry Cate <ncate@alumni.princeton.edu>
2021-01-20 23:57:42 -0500

    Delete "open" command and change "begin" to accept an arbitrary message ID.
    Since writing the "open" command, I realized that anyone can get a
    message ID by clicking (you guessed it) "Copy ID" in the menu for that
    message. That means we can just pass in the ID of the message to check,
    and can also delete a lot of global state. (Mapping guilds to message
    IDs, for example, since we no longer need to track that..)

--- main.py
+++ main.py
@@ -24,69 +24,39 @@ if CHALLONGE_TOKEN_VAR not in os.environ:
         CHALLONGE_TOKEN_VAR))
 challonge_auth = os.environ[CHALLONGE_TOKEN_VAR]
 
-
 # Create bot instance.
 bot = commands.Bot(command_prefix=PREFIX)
-tournaments_by_guild_id = {}
-
 
 @bot.event
 async def on_ready():
     print('sup')
 
+class WrappedMessage(discord.ext.commands.MessageConverter):
+    """
+    Behaves exactly the same as discord.py's MessageConverter,
+    but sends back an error message if the message cannot be found.
+    """
+    async def convert(self, ctx, argument):
+        try:
+            return await super().convert(ctx, argument)
+        except discord.ext.commands.MessageNotFound:
+            await ctx.send(f"Unable to find message {argument}. (Remember to hold shift when clicking 'Copy ID' to get the FULL ID. It should have a dash in the middle.)")
 
 @bot.command()
-async def open(ctx, dest_channel=None, message="Registration is open! React to this message to register."):
+async def begin(ctx, reg_msg: WrappedMessage, tourney_name="Tournament"):
     """
-    Posts a message that discord members can react to to be entered into a (thus far un-created) tournament.
-
-    Args:
-        dest_channel: The channel that the registration message should be posted to.
-            Defaults to whichever channel this command was run from.
-        message: The content of the registration message.
-    """
-    guild = ctx.message.channel.guild
-
-    # To keep things simple, only allow one tournament per guild for now.
-    if guild.id in tournaments_by_guild_id:
-        # TODO give option of cancelling. Eventually allow many tournaments per 'cord.
-        await ctx.message.channel.send("Sorry, a tournament is already being run from this discord.")
-        return
-
-    # If the destination channel is invalid, just send the message to the
-    # channel the message was received from.
-    dest = ctx.message.channel
-    for c in guild.channels:
-        if c.name == dest_channel and isinstance(c, discord.TextChannel):
-            dest = c
-            break
-
-    sent_msg = await dest.send(message)
-    tournaments_by_guild_id[guild.id] = Tournament(
-        guild.id, sent_msg.channel.id, sent_msg.id)
-
-    await ctx.send("Registration message posted!")
-
-
-@bot.command()
-async def begin(ctx, tourney_name="Tournament"):
-    """
-    Creates a bracket containing every member that reacted to the previously-posted registration message.
+    Creates a bracket containing every member that reacted to the specified reg_msg.
     Responds with a link to the new bracket.
 
     Args:
+        reg_msg: The message to check for reactions.
+            If it is not in the same channel as the begin command was run in,
+            it must be in the <channel ID>-<msg ID> format.
         tourney_name: The title of the tournament.
     """
-    guild = ctx.message.channel.guild
-    tournament = tournaments_by_guild_id.get(guild.id)
-    if tournament is None:
-        await ctx.send(f"No tournament to begin! Start a tournament with {PREFIX}open <channel> <message>")
-        return
-
     # Collect all the users who reacted to the registration message.
-    reg_message = await guild.get_channel(tournament.channel_id).fetch_message(tournament.registration_message_id)
     names_by_discord_id = {}
-    for r in reg_message.reactions:
+    for r in reg_msg.reactions:
         async for u in r.users():
             names_by_discord_id[u.id] = f'{u.name}#{u.discriminator}'
 
@@ -95,6 +65,7 @@ async def begin(ctx, tourney_name="Tournament"):
     bracket.add_players(names_by_discord_id.values())
 
     await ctx.send(f"Bracket has been created! View it here: {bracket.link}")
+
 
 # Log in and begin reading and responding to messages.
 # Nothing else will run below this line.
