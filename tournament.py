1ff7ca0f1d496c98e00117bc38e88f379fcd22f4
Perry Cate <NPCate01@gmail.com>
2021-02-03 18:43:13 -0800

    Add basic TourneyState object.
    This will be used to keep track of which matches have already been
    pinged.
    
    Currently it doesn't actually use the backup file thing, we'll need to
    do that later.

--- tournament.py
+++ tournament.py
@@ -1,6 +1,48 @@
-# Manages state of a tournament being run from within a discord server ("guild").
-class Tournament:
-    def __init__(self, guild_id, channel_id, msg_id):
-        self.guild_id = guild_id
-        self.channel_id = channel_id
-        self.registration_message_id = msg_id
+import pickle
+
+from os import path
+
+
+STATE_BACKUP_DIR = '/tmp/'
+
+
+class TourneyState:
+    """
+    Manages state of a tournament being run.
+    This class backs info up in a nonvolatile way.
+    """
+
+    # TODO next:
+    # Load this from file if present
+    # see note in main.py
+
+    def __init__(self, tournament_id: str):
+        self._tournament_id = tournament_id
+        self._called_matches = set()
+
+        # Will blow up if 2 bots are managing the same tournament.
+        # Things would blow up if you had that happening anyway.
+        save_file_name = f'{STATE_BACKUP_DIR}{self.tournament_id}'
+
+        # Read state if possible.
+        if path.exists(save_file_name):
+            save_file = open(save_file_name, 'r')
+            self = pickle.load(save_file)
+            return
+
+        self._save_file = open(save_file_name, 'w')
+
+    def tournament_id(self):
+        return self._tournament_id
+
+    def mark_called(self, match_id):
+        self._called_matches.add(match_id)
+        self._save()
+
+    def was_called(self, match_id):
+        return match_id in self._called_matches
+
+    def _save(self):
+        # TODO
+        #pickle.dump(self, self._save_file)
+        self._save_file.flush()
